67318e800c973400b96ac5bbea2da1a716d1e3e4
Michael T <66504185+michaeltukdev@users.noreply.github.com>
2024-03-28 21:27:32 +0000

    Base route & Tailwind
    Removing the welcome page just to have a "home" page.
    
    Also included Tailwind and all tailwind required stuff.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Home</title>
+
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
+</body>
+
+</html>
