7d0635b022da34908f7f4c0f834d4b4fd24cc75d
Michael T <66504185+michaeltukdev@users.noreply.github.com>
2024-04-01 03:06:13 +0100

    Colors
    Default colours for the application added to the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ export default {
     "./resources/**/*.js",
   ],
   theme: {
+    colors: {
+      'background': '#18181B',
+      'container': '#212125',
+      'input': '#2E2E33',
+      'primary-text': '#FFFFFF',
+      'secondary-text': '#B1B1B1',
+      'primary': '#FF6F8C', 
+      'secondary': '#E24C6A',
+    },
     extend: {},
   },
   plugins: [],
