921689d252b9970478b95f8a0f4a01dbd2cec608
Michael T <66504185+michaeltukdev@users.noreply.github.com>
2024-04-02 21:11:11 +0100

    Removing border for now
    The border wasn't exactly what I wanted to do in the first place so for now I'll just get rid of it.

--- resources/views/livewire/resources-feed.blade.php
+++ resources/views/livewire/resources-feed.blade.php
@@ -1,11 +1,7 @@
-<div x-data="{ openItem: null }">
+<div>
     @foreach ($resources as $index => $resource)
-        <div
-            @click.prevent="
-                openItem = openItem === {{ $index }} ? null : {{ $index }};
-                $dispatch('selectResource', { resourceId: {{ $resource->id }} });
-            ">
-            <div class="resource" :class="openItem === {{ $index }} ? 'border-red' : ''">
+        <div @click.prevent="$dispatch('selectResource', { resourceId: {{ $resource->id }} });">
+            <div class="resource">
                 <div class="flex items-center justify-between">
                     <p class="resource-title">{{ $resource->name }}</p>
                     <p class="category mt-[-3px]">{{ $resource->category->name }}</p>
